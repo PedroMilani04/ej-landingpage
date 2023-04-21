c8fbe0a9a79a82af90c4cf2117010681ee918fbc
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-04-20 22:00:16 -0300

    update test

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -4,16 +4,18 @@ function Info() {
     <div className="info">
       <img className='unespimg' src='https://s2.glbimg.com/G3oHthkpW6iBumdW0c62Ar7Im98=/0x0:620x465/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2014/04/03/unesp.jpg' alt='Logo da Unesp' />
       <div className='contentinfo'>
+      <img className='unespimg1' src='https://s2.glbimg.com/G3oHthkpW6iBumdW0c62Ar7Im98=/0x0:620x465/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2014/04/03/unesp.jpg' alt='Logo da Unesp' />
+
         <h1>Sobre a FCT - UNESP</h1>
         <h2>Por quê depositar sua confiança?</h2>
-        <p>um dolor sit alor laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        <p>um dolote velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           "Lorem ipsum dolor sit amet, consectetur</p>
-          <p>um doloolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          <p>um doloolore mollit anim id est laborum."
           "Lorem ipsum dolor sit amet, consectetur</p>
         <ul>
-          <li>Item 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1</li>
-          <li>Item 2tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1</li>
-          <li>Item 3tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1tem 1</li>
+          <li className='a12'>Best prices in the region</li>
+          <li>Best prices in the region</li>
+          <li>Best prices in the region</li>
         </ul>
       </div>
 
