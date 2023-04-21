9b7bbae2b8054ee692d0ee3589a19a14a049bcc3
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-04-21 12:53:34 -0300

    method + responsiveness added

--- src/Method/Method.js
+++ src/Method/Method.js
@@ -4,7 +4,30 @@ import { } from "./Method.css";
 function Method() {
   return (
     <div className="method">
-      <h1>Method</h1>
+      <div className="method-content">
+        <h1>Nosso método de trabalho</h1>
+        <h2>Abrindo as portas da tecnologia. De uma a uma.</h2>
+        <div className="cards">
+          <section className="card">
+            <h3>1. Aprenda</h3>
+            <h4>Lorem ipsuim do lipsuim doloar ameoroem ipsum</h4>
+            <p>Lorem ipsuim doloar amet amt dolror loroem ipsum</p>
+          </section>
+          <section className="card">
+            <h3>1. Aprenda</h3>
+            <h4>Lorem ipsuim do loroem ipsum</h4>
+            <p>Lorem ipsuim doloar ipsuim doloar ameipsuim doloar ameamet amt dolror loroem ipsum</p>
+          </section>
+          <section className="card">
+            <h3>1. Aprenda</h3>
+            <h4>Lorem ipsuim do loroem ipsum</h4>
+            <p>Lorem ipsuim doloar amipsuipsuim doloar ameipsuim doloar ameim doloar ameet amt dolror loroem ipsum</p>
+          </section>
+        </div>
+        <div>
+          <button className="button-74">Contact us</button>
+        </div>
+      </div>
     </div>
   );
 }
